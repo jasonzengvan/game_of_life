dfb86caa1622ab6f36b7e6812b3fcc5495d97b82
Jason Zeng <zengzixuanca@gmail.com>
2017-05-01 00:39:20 -0700

    implement click(), start(), stop()

--- game_of_life.js
+++ game_of_life.js
@@ -1,24 +1,32 @@
 angular
 .module('gameOfLife', [])
-.controller('GameController', function() {
-	var game = this;
-	var N = 50;
+.controller('GameController', function($interval) {
 
+	var N = 50;
+	var TIMER = 50;
+
+	var game = this;
+	var stop;
+	
 	// board is a N x N matrix
 	game.board = new Array(N);
 	for (var i = 0; i < N; i++) {
 		game.board[i] = new Array(N).fill(false);
 	}
-	for (var i = 20; i < 30; i++) game.board[24][i] = true;
 
 	// TODO: start auto stepping
 	game.start = function() {
+		if (angular.isDefined(stop)) return;
 
+		stop = $interval(game.step, TIMER);
 	};
 
 	// TODO: stop auto stepping
 	game.stop = function() {
-
+		if (angular.isDefined(stop)) {
+			$interval.cancel(stop);
+			stop = undefined;
+		}
 	};
 
 	// reset the board to initial state
@@ -49,7 +57,6 @@ angular
 				if (i - 1 >= 0 && j + 1 < N && game.board[i-1][j+1]) nb++;
 				if (j + 1 < N && game.board[i][j+1]) nb++;
 				if (i + 1 < N && j + 1 < N && game.board[i+1][j+1]) nb++;
-				console.log("i = " + i + ", j = " + j + ", nb = " + nb);
 
 
 				if ((nb == 2 && game.board[i][j])|| nb == 3) next[i][j] = true;
