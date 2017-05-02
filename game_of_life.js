ba9c49521075530650880f24e63d64223ec55196
Jason Zeng <zengzixuanca@gmail.com>
2017-05-01 19:36:28 -0700

    add select() & etc.

--- game_of_life.js
+++ game_of_life.js
@@ -2,45 +2,102 @@ angular
 .module('gameOfLife', [])
 .controller('GameController', function($interval) {
 
-	var N = 50;
-	var TIMER = 50;
-
 	var game = this;
+	var N = 50;
+	var interval = 150;
 	var stop;
-	
-	// board is a N x N matrix
-	game.board = new Array(N);
-	for (var i = 0; i < N; i++) {
-		game.board[i] = new Array(N).fill(false);
-	}
 
-	// TODO: start auto stepping
+	// board is a N x N matrix
+	game.board = getEmptyBoard(N);
+
+	// collection of known game patterns
+	game.collection = [
+		{
+			name: "Empty",
+			width: 0,
+			height: 0,
+			data: []
+		},
+		{
+			name: "Glider",
+			width: 3,
+			height: 3,
+			data: [[0, 2], [1, 0], [1, 2], [2, 1], [2, 2]]
+		},
+		{
+			name: "10 Cell Row", 
+			width: 10,
+			height: 1,
+			data: [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7], [0, 8], [0, 9]]
+		},
+		{
+			name: "Tumbler", 
+			width: 7,
+			height: 6,
+			data: [[0, 0], [0, 1], [0, 5], [0, 6], [1, 0], [1, 2], [1, 4], [1, 6], [2, 0], [2, 2], [2, 4], [2, 6], [3, 2], [3, 4], [4, 1], [4, 2], [4, 4], [4, 5], [5, 1], [5, 2], [5, 4], [5, 5]]
+		},
+		{
+			name: "4-8-12 Diamond", 
+			width: 12,
+			height: 9,
+			data: [[0, 4], [0, 5], [0, 6], [0, 7], [2, 2], [2, 3], [2, 4], [2, 5], [2, 6], [2, 7], [2, 8], [2, 9], [4, 0], [4, 1], [4, 2], [4, 3], [4, 4], [4, 5], [4, 6], [4, 7], [4, 8], [4, 9], [4, 10], [4, 11], [6, 2], [6, 3], [6, 4], [6, 5], [6, 6], [6, 7], [6, 8], [6, 9], [8, 4], [8, 5], [8, 6], [8, 7]]
+		}
+	];
+	game.selected = game.collection[0];
+	game.isRunning = false;
+	game.generations = 0;
+
+
+	// start auto stepping
 	game.start = function() {
 		if (angular.isDefined(stop)) return;
 
-		stop = $interval(game.step, TIMER);
+		stop = $interval(game.step, interval);
+		game.isRunning = true;
 	};
 
-	// TODO: stop auto stepping
+	// stop auto stepping
 	game.stop = function() {
 		if (angular.isDefined(stop)) {
 			$interval.cancel(stop);
 			stop = undefined;
+			game.isRunning = false;
 		}
 	};
 
 	// reset the board to initial state
 	game.clear = function() {
-		game.board = new Array(N).fill(new Array(N).fill(false));
+		game.stop();
+		game.board = getEmptyBoard(N);
+		game.generations = 0;
+	};
+
+	// select a pattern from collection
+	game.select = function() {
+		var pattern = game.selected;
+		var x = Math.floor((N - pattern.width) / 2);
+		var y = Math.floor((N - pattern.height) / 2);
+		console.log(x+ " "+ y);
+		game.clear();
+		pattern.data.forEach(function(cell) {
+			game.board[y + cell[0]][x + cell[1]] = true;
+		});
+		game.generations = 0;
 	};
 
 	// change the boolean value of game.board[x][y]
 	game.click = function(x, y) {
 		game.board[x][y] = !game.board[x][y];
+		game.generations = 0;
 	};
 
-	// TODO: step a single life emulation
+
+	// TODO: optimize the algorithm
+	// step through a single life iteration
+	// Rule 1: If a cell is populated it survives iff it has 2 or 3 neighbors.
+	// Rule 2: If a cell is unpopulated it becomes populated iff it has 3 neighbors.
 	game.step = function() {
+
 		var next = new Array(N);
 		for (var i = 0; i < N; i++) {
 			next[i] = new Array(N).fill(false);
@@ -65,7 +122,15 @@ angular
 		}
 
 		game.board = next;
+		game.generations ++;
+	};
 
+	function getEmptyBoard(size) {
+		var board = new Array(size);
+		for (var i = 0; i < size; i++) {
+			board[i] = new Array(size).fill(false);
+		}
+		return board;
 	};
 });
 
